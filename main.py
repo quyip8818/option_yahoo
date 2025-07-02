c7d8b856a36a1b605fc282aad051df9805ebc03e
qujianguo1956 <qujianguo1956@gmail.com>
2025-07-01 23:18:12 -0400

    fix

--- main.py
+++ main.py
@@ -4,16 +4,11 @@ from src.analysis.option_qualified import filter_qualified_options
 from src.quandl.option_percentiles import fetch_option_percentiles
 from src.utils.date_utils import get_last_trading_day
 
-today = datetime.date(2025, 6, 27)
-# today = get_last_trading_day()
+# today = datetime.date(2025, 6, 27)
+today = get_last_trading_day()
 
 SKIP_SYMBOLS = {}
 
 fetch_option_percentiles(today)
 filter_qualified_options(today)
 # fetch_all_yf_options(today, get_last_iv_rank(), SKIP_SYMBOLS)
-
-today = get_last_trading_day()
-fetch_option_percentiles(today)
-filter_qualified_options(today)
-
