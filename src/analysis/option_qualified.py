839843edd3af3b5d608df988c4f724585daf38ef
quyip <quyip@hotmail.com>
2025-07-21 21:26:15 -0700

    fix

--- src/analysis/option_qualified.py
+++ src/analysis/option_qualified.py
@@ -13,7 +13,6 @@ def filter_qualified_options(date):
         return
     df = pd.read_csv(get_quandl_path(f"option_iv_rank/{date_path}.csv"))
     df = df[df.apply(is_qualified, axis=1)]
-    # df = df[df['ivcall1080_rank'] <= 50 & df['ivput1080_rank'] <= 60]
 
     df.to_csv(get_quandl_path(f"option_qualified/{date_path}.csv"), index=False)
 
@@ -21,16 +20,20 @@ def filter_qualified_options(date):
 def is_qualified(row):
     if row['max_days'] < 500 or np.isnan(row['max_days']):
         return False
-    if row['current_price'] < 20:
+    if row['current_price'] < 30:
         return False
     if row['ivmean1080'] > 0.4:
         return False
-    if row['ivmean1080'] > 0.2:
-        if row['ivcall1080_rank'] > 30 or row['ivput1080_rank'] > 40:
+    if np.isnan(row['ivcall1080_rank']) or np.isnan(row['ivput1080_rank']):
+        if row['iv_ratio'] > 0.8 or np.isnan(row['iv_ratio']):
             return False
     else:
-        if row['ivcall1080_rank'] > 70 or row['ivput1080_rank'] > 70:
+        if row['ivmean1080'] > 0.2:
+            if row['ivcall1080_rank'] > 8 or row['ivput1080_rank'] > 12:
+                return False
+        else:
+            if row['ivcall1080_rank'] > 10 or row['ivput1080_rank'] > 20:
+                return False
+        if row['iv_ratio'] > 1.0 or np.isnan(row['iv_ratio']):
             return False
-    if row['iv_ratio'] > 0.75 or np.isnan(row['iv_ratio']):
-        return False
     return  True
