71d3a4cdecbf04e5234ce98fa8db09bc82c25afc
quyip <quyip@hotmail.com>
2025-08-25 16:50:08 -0700

    fix

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from src.analysis.option_qualified import filter_qualified_options
 from src.quandl.option_percentiles import fetch_option_percentiles
 from src.utils.date_utils import get_last_trading_day
 
-today = datetime.date(2025, 8, 22)
-# today = get_last_trading_day()
+# today = datetime.date(2025, 8, 25)
+today = get_last_trading_day()
 
 SKIP_SYMBOLS = {}
 
